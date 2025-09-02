bba128992890d401c1c066ba8dab583e695b45b1
Md. Abu Sayam <abusayam.dev@gmail.com>
2025-09-02 09:24:10 +0600

    multiple

---
+++ lib/multiple.dart
@@ -0,0 +1,12 @@
+int add (int a, int b){
+  return a + b;
+}
+
+int subtract(int a , int b){
+  return a - b;
+}
+
+void main (){
+  print("Addition = ${add(5, 8)}");
+  print("Subtraction = ${subtract(10, 6)}");
+}
